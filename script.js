147b20879528ae14d2e3942655c9e9f24ed38157
Adriel Hahn <adrielhahn@hotmail.com>
2022-04-21 01:21:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,178 @@
+function rede(){
+     let rede = document.getElementById('func');
+        rede.classList.add('revert-top');
+    
+}
+
+function redeTwo(){
+    let rede = document.getElementById('funcTwo');
+        rede.classList.add('revert-top');
+   
+}
+
+
+function redeTh(){
+    let rede = document.getElementById('funcTh');
+    let project = document.getElementById('project');
+        rede.classList.add('revert-top');
+        project.style.height = '100%';
+}
+
+
+function redeFo(){
+    let rede = document.getElementById('funcFo');
+        rede.classList.add('revert-top');
+   
+}
+
+function pass(){
+    let passar = document.getElementById('pass');
+
+    passar.classList.remove("passExit");
+    passar.style.display = "flex";
+    passar.classList.add("passFunc");
+
+}
+
+function passX(){
+    let sair = document.getElementById('pass');
+    
+
+    
+
+    sair.classList.remove("passFunc");
+    sair.classList.add("passExit");
+    
+}
+
+
+function passTw(){
+    let passar = document.getElementById('passTw');
+
+    passar.classList.remove("passExit");
+    passar.style.display = "flex";
+    passar.classList.add("passFunc");
+
+}
+
+function passXtw(){
+    let sair = document.getElementById('passTw');
+
+    
+
+    sair.classList.remove("passFunc");
+    sair.classList.add("passExit");
+    
+}
+
+function passTh(){
+    let passar = document.getElementById('passTh');
+
+    passar.classList.remove("passExit");
+    passar.style.display = "flex";
+    passar.classList.add("passFunc");
+
+}
+
+function passXth(){
+    let sair  = document.getElementById('passTh');
+
+    
+
+    sair.classList.remove("passFunc");
+    sair.classList.add("passExit");
+    
+}
+
+function passFo(){
+    let passar = document.getElementById('passFo');
+
+    passar.classList.remove("passExit");
+    passar.style.display = "flex";
+    passar.classList.add("passFunc");
+
+}
+
+function passXFo(){
+    let sair  = document.getElementById('passFo');
+
+    
+
+    sair.classList.remove("passFunc");
+    sair.classList.add("passExit");
+    
+}
+
+function passFi(){
+    let passar = document.getElementById('passFi');
+
+    passar.classList.remove("passExit");
+    passar.style.display = "flex";
+    passar.classList.add("passFunc");
+
+}
+
+function passXfi(){
+    let sair  = document.getElementById('passFi');
+
+    
+
+    sair.classList.remove("passFunc");
+    sair.classList.add("passExit");
+    
+}
+
+function passSi(){
+    let passar = document.getElementById('passSi');
+
+    passar.classList.remove("passExit");
+    passar.style.display = "flex";
+    passar.classList.add("passFunc");
+
+}
+
+function passXsi(){
+    let sair  = document.getElementById('passSi');
+
+    
+
+    sair.classList.remove("passFunc");
+    sair.classList.add("passExit");
+    
+}
+
+function passNi(){
+    let passar = document.getElementById('passNi');
+
+    passar.classList.remove("passExit");
+    passar.style.display = "flex";
+    passar.classList.add("passFunc");
+
+}
+
+function passXni(){
+    let sair  = document.getElementById('passNi');
+
+    
+
+    sair.classList.remove("passFunc");
+    sair.classList.add("passExit");
+    
+}
+
+function menuM(){
+    let aside = document.getElementById('mobile-menu');
+    let nav  = document.getElementById('mobile-nav');
+    
+    aside.style.display = "flex";
+    nav.style.display = "flex";
+}
+
+function exitMenu(){
+    let aside = document.getElementById('mobile-menu');
+    let nav  = document.getElementById('mobile-nav');
+    
+
+    aside.style.display = "none";
+    nav.style.display = "none";
+}
